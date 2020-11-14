d013ed2ff4bb28d7130e16d1cb6f903096f4ee81
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-11-14 12:06:07 +0100

    disable cursor pointer and select highlight

--- hangman.js
+++ hangman.js
@@ -4,7 +4,7 @@ const letters = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ";
 const create_keyboard = () => {
     let keyboard_content = "";
     for (let i = 0; i < 35; i++) {
-        keyboard_content += "<div class='letter' onclick='check_letter(" + i + ")'>" + letters[i] + "</div> ";
+        keyboard_content += "<div class='letter' id=" + i + " onclick='check_letter(" + i + ")'>" + letters[i] + "</div> ";
     }
     document.getElementById("keyboard").innerHTML = keyboard_content;
 }
@@ -26,9 +26,9 @@ const print_passwd = () => {
 }
 
 const check_letter = (letter) => {
+    //replace dashes with letters
     let indexes = [];
     let buf;
-
     for (let i = passwd.length - 1; i >= 0; i--) {
         if (passwd.charAt(i) === letters[letter] && buf !== i) {
             indexes.push(i);
@@ -38,7 +38,10 @@ const check_letter = (letter) => {
     for (let i = 0; i < indexes.length; i++) {
         hashed_passwd = hashed_passwd.substring(0, indexes[i]) + letters[letter] + hashed_passwd.substring(indexes[i] + 1);
     }
+    //update passwd
     print_passwd();
+    //
+    document.getElementById(letter).style.cursor = "default";
 }
 
 
